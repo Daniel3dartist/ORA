31534f95c013b979a13055e7aea509140e7afd99
Daniel3dartist <danielsobrinho3dartist@gmail.com>
2024-03-20 20:57:03 -0300

    small updates

--- api/urls.py
+++ api/urls.py
@@ -23,8 +23,8 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('', include('core.urls')),
-    path('', include('fake_gen.urls')),
+    path('api/v1/', include('core.urls')),
+#    path('v1/', include('fake_gen.urls')),
     path('admin/', admin.site.urls),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from fake_gen import urls
 
 urlpatterns = [
-    path('api/accounts/', include('django.contrib.auth.urls')),
-    path('api/items/', views.items, name='items'),
-    path('api/sing-up/', views.sing_up, name='sing_up'),
-    path('api/users/', views.users, name='users'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('items/', views.items, name='items'),
+    path('sing-up/', views.sing_up, name='sing_up'),
+    path('users/', views.users, name='users'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('fake_gen/', include(urls))
 ]
